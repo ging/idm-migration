35b35275410c129b52358a77d66365e405704e85
aalonsog <aalonsog@dit.upm.es>
2015-03-25 18:59:44 +0100

    Added all script

---
+++ run_all_scripts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+node scripts/users.js -->logs/users.log
+node scripts/orgs.js -->logs/orgs.log
+node scripts/apps.js -->logs/apps.log
+node scripts/ruser_org.js -->logs/ruser_org.log
+node scripts/roles.js -->logs/roles.log
+node scripts/rusers.js -->logs/rusers.log
+node scripts/perms.js -->logs/perms.log
+node scripts/rperms.js -->logs/rperms.log
+node scripts/horizon_admins.js -->logs/horizon_admins.log
+node scripts/keystone_users.js -->logs/keystone_users.log
+node scripts/service_catalogue.js -->logs/service_catalogue.log
